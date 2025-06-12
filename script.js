393bd67c4751dd6eb6d2fbd8012b0967362bddc9
Vansh Singh <0vanshsingh0gmail.com>
2025-06-12 13:20:16 +0530

    Initial commit for Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Welcome to Vansh Singh's Portfolio!");
+// Dark Mode Toggle
+document.getElementById("darkModeToggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
